56cc6cf84b0afdca09281b052a736e7b38f037bf
Ferencz Magyari <afmagyari@gmail.com>
2017-10-14 12:22:36 -0700

    improve minor formatting

--- user.js
+++ user.js
@@ -1,34 +1,36 @@
 
 var url = 'https://randomuser.me/api/';
 
-document.getElementById('btn').onclick = function() {
+
+var btn = document.querySelector('#btn');
+btn.addEventListener('click', function() {
 	fetch(url)
 		.then(handleErrors)
 		.then(parseJSON)
 		.then(updateProfile)
-		.catch(printError)	
-}
+		.catch(printError);
+});
+
 
 	function handleErrors(res) {
 		if (!res.ok) {
 			throw Error("Oops! An error happened");
 		}
-		return res.json();
+		return res;
 	}
 
-	function parseJSON(data) {
-		return data.results[0];
+	function parseJSON(res) {
+		return res.json().then(function(data) {
+			return data.results[0];	
+		})
 	}
 
 	function updateProfile(user) {
-		console.log(user);
 		document.getElementById('avatar').src = user.picture.medium;
 		document.getElementById('fullname').innerHTML = user.name.first + " " + user.name.last;
 		document.getElementById('username').innerHTML = user.login.username;
 		document.getElementById('email').innerHTML = user.email;
 		document.getElementById('city').innerHTML = user.location.city;
-		
-		return user;
 	}
 
 	function printError(err) {
