d3f839aa5e3131c6f4371983f697393055a84582
Ferencz Magyari <afmagyari@gmail.com>
2017-10-14 11:34:23 -0700

    create basic layout and error handleing

--- user.js
+++ user.js
@@ -1,1 +1,26 @@
-// https://randomuser.me/api/
+
+var url = 'https://randomuser.me/api/';
+fetch(url)
+	.then(handleErrors)
+	.then(parseJSON)
+	.then(updateProfile)
+	.catch(printError)
+
+	function handleErrors(req) {
+		if (!req.ok) {
+			throw Error("Error fetching request");
+		}
+		return req;
+	}
+
+	function parseJSON() {
+
+	}
+
+	function updateProfile() {
+
+	}
+
+	function printError(err) {
+		console.log(err);
+	}
